3ab03fc02b97ba7b4f2409ffa43d5f47babfd018
allen spayne <mainacc.salapayne@gmail.com>
2024-07-05 14:03:20 +0800

    chore: Add shadcn-ui and React Query Builder dependencies

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "../../styles/globals.css";
+
+export const metadata: Metadata = {
+  title: "Healthcare / Mixpanel",
+  description: "this is the User clone Interface for the Mixpanel Healthcare project to demonstrate the React Query build. this is a test project only and not intend to use in production.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { QueryBuilder } from 'react-querybuilder';
+import 'react-querybuilder/dist/query-builder.css';
+
+export default function Page() {
+  return(
+
+    <QueryBuilder />
+  )
+}
